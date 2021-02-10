0996a7fba206c7b79b7d0b3176d9bb62dc208f00
musaabdullah <musaabdalwahed@gmail.com>
2021-02-10 09:03:06 +0200

    Add ejs to the project

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejs = require('ejs');
 const postRouter = require('./routers/posts');
 const port = process.env.PORT | 3000;
 app.use(cors('dev'));
@@ -12,6 +13,7 @@ app.use('/public',express.static(path.join(__dirname,'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('combined'));
+app.set('view engine', 'ejs');
 
 mongoose.connect("mongodb://localhost:27017/hell", {
     useCreateIndex: true,
--- routers/posts.js
+++ routers/posts.js
@@ -6,20 +6,24 @@ const Post = require('../model/posts');
 router.get('/posts', async (req, res) => {
   try {
       const  { page, limit }  =  req.query ;
-    //   const limit = 7;
 
-      const post = await Post.find({}).limit(limit * 1).skip((page - 1) * limit).exec();
+
+      const posts = await Post.find({}).limit(limit * 1).skip((page - 1) * limit).exec();
       const count = await Post.countDocuments();
-      res.json({
-          totalPages: Math.ceil(count / limit),
-          currentPage: page,
-          post,
-        })
+    //   res.json({
+    //       totalPages: Math.ceil(count / limit),
+    //       currentPage: parseInt(page),
+    //       post,
+    //     })
+    res.render('index', {totalPages: Math.ceil(count / limit), currentPage: parseInt(page), posts})
   } catch(error) {
       res.json(error);
   }
 })
 
+// router.get('/index', (req, res) => {
+//     res.render('index');
+// })
 
 
 module.exports = router;
